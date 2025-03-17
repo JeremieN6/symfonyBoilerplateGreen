00035f2244afea1354fefe535b0b2178cecf2631
Jeremie <ngoyi.jeremie@gmail.com>
2025-03-17 21:07:47 +0100

    Add : Ajout de la sécurité sur /admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -13,6 +13,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -20,6 +21,10 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         // return parent::index();
+
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('Accès refusé.');
+        }
 
         return $this->render('admin/dashboard.html.twig');
 
